0216ce8eba3f8929de45939489ba70a8a3fd19bf
Tursun <alhini4ek@gmail.com>
2026-03-13 18:53:03 +0500

    Post

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,28 +1,86 @@
-import { use, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import HomeTemplate from './Components/homeTemplate'
 import styles from './MainPage.module.css'
-import { getListHomes } from './service/service'
+import { getListHomes, sendHomeData } from './service/service'
 
 
 const MainPage = () => {
-    const [home, setHome] = useState([])
+  const [home, setHome] = useState([])
 
-    useEffect(() => {
-      getListHomes().then((res) => setHome(res.data))
-    }, [])
+  useEffect(() => {
+    getData()
+  }, [])
+
+  const getData = () => {
+    getListHomes().then((res) => setHome(res.data))
+  }
+
+  const [payload, setPayload] = useState({
+    name: '',
+    price: 0,
+    img: "./home1.svg"
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setPayload({
+      ...payload,
+      [name]: value,
+    });
+  };
+
+  const handleSend = async() => {
+    try {
+      if (
+        payload.name === "" ||
+        payload.name === undefined ||
+        payload.name === null
+      ) {
+        alert("Введите название дома");
+      } else {
+         await sendHomeData(payload)
+        setPayload({
+          name: '',
+          price: 0,
+          img: "./home1.svg"
+        })
+        await getData()
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  };
 
   return (
     <div className={styles.MainPage}>
-      <h1 className={styles.title}>Строительство коттеджей</h1>
+      <div className={styles.top__container}>
+        <h1 className={styles.title}>Строительство коттеджей</h1>
+        <div className={styles.form}>
+          <input
+            placeholder="Название>.."
+            value={payload.name}
+            onChange={handleChange}
+            name="name"
+          />
+          <input
+            placeholder="Цена>.."
+            type="number"
+            value={payload.price}
+            onChange={handleChange}
+            name="price"
+          />
+          <button onClick={handleSend}>Отправить</button>
+        </div>
+      </div>
       <div className={styles.wrapper}>
         {
-            home.map((h)=> (
-                <HomeTemplate name={h.name} img={h.img} key={h.id}/>
-            ))
+          home.map((h) => (
+            <HomeTemplate name={h.name} img={h.img} key={h.id} />
+          ))
         }
       </div>
     </div>
   )
 }
 
-  export default MainPage
+export default MainPage
--- src/pages/MainPage/service/service.js
+++ src/pages/MainPage/service/service.js
@@ -4,3 +4,7 @@ import { BASE_URL } from "../../../shared/const/const"
 export const getListHomes = () => {
     return openApi(BASE_URL).get('/homes')
 }
+
+export const sendHomeData = (payload) => {
+    return openApi(BASE_URL).post('/homes', payload)
+}
