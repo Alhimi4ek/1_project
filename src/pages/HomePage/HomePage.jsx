c9b6419bf6bee2415215003baeb17706b6800868
Tursun <alhini4ek@gmail.com>
2026-03-27 19:55:00 +0500

    PUT

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,20 +1,84 @@
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 import { getHomeById } from "./service/service";
 import { useParams } from "react-router-dom";
+import { patchHome } from "./service/service";
 import styles from "./HomePage.module.css"
 
 const HomePage = () => {
-    const {id} = useParams();
-    const [home, setHome] = useState({name: "", img: "", price: 0});
-        useEffect(() => {
+    const { id } = useParams();
+    const [home, setHome] = useState({ name: "", img: "", price: 0 });
+    const [isEdit, setIsEdit] = useState(false);
+    useEffect(() => {
         getHomeById(id).then((res) => setHome(res.data));
     }, []);
-    return (
-        <div className={styles.home}>
-            <img src={home.img} alt="home" />
-            <p>Дом: {home.name}</p>
-            <p>Цена: {home.price}</p>
-        </div>
-    );
+
+
+    const [payload, setPayload] = useState({
+        name: '',
+        price: 0,
+    });
+
+const handleChange = (e) => {
+    const { name, value } = e.target;
+    setPayload({
+        ...payload,
+        [name]: value,
+    });
 };
-export default HomePage;
+
+const patchHandler = async () => {
+    if (
+        payload.name === "" ||
+        payload.name === undefined ||
+        payload.name === null
+    ) {
+        alert("Введите название дома");
+    } else {
+        await patchHome(id, payload);
+        await getHomeById(id).then((res) => setHome(res.data));
+        setIsEdit(false)
+    }
+};
+
+return (
+    <div className={styles.home}>
+        <img src={home.img} alt="home" />
+
+        {
+            isEdit ? (
+                <div className={styles.isEdit}>
+                    <input
+                        placeholder="Название..."
+                        value={payload.name}
+                        onChange={handleChange}
+                        name="name"
+                    />
+                    <input
+                        placeholder="Цена..."
+                        value={payload.price}
+                        onChange={handleChange}
+                        name="price"
+                    />
+                    <div className={styles.buttons}>
+                        <button onClick={patchHandler}>Сохранить</button>
+                        <button onClick={() => setIsEdit(false)}>Отмена</button>
+                    </div>
+                </div>
+            ) : (
+                <>
+                    <p>Название: {home.name}</p>
+                    <p>Цена: {home.price}</p>
+                    <div className={styles.buttons}>
+                        <button onClick={() => setIsEdit(true)}>Редактировать</button>
+                        <button>Удалить</button>
+                    </div>
+                </>
+            )
+        }
+
+
+    </div>
+);
+};
+
+export default HomePage
--- src/pages/HomePage/service/service.js
+++ src/pages/HomePage/service/service.js
@@ -4,3 +4,7 @@ import { BASE_URL } from "../../../shared/const/const"
 export const getHomeById = (id) => {
     return openApi(BASE_URL).get(`/homes/${id}`)
 }
+
+export const patchHome =(id, payload) => {
+    return openApi(BASE_URL).put(`/homes/${id}`, payload)
+}
