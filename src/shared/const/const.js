5e75b4d57f331246fb3a03170a38463d22bead61
Tursun <alhini4ek@gmail.com>
2026-04-07 17:37:39 +0500

    json server

--- src/shared/const/const.js
+++ src/shared/const/const.js
@@ -1,1 +1,2 @@
-export const BASE_URL = 'https://69b3ccfde224ec066bdd2558.mockapi.io/'
+// export const BASE_URL = 'https://69b3ccfde224ec066bdd2558.mockapi.io/'
+export const BASE_URL = 'http://localhost:8080/'
